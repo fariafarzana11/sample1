4ab21be380297265ef7d6fca74d53d5bbb3862fb
fariafarzanamukta <128606812+fariafarzana11@users.noreply.github.com>
2023-07-31 11:50:47 +0600

    Add files via upload
    hgggg

---
+++ image.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+import cv2
+
+
+img1 = cv2.imread("H:\data science\Image Processing\download.jpg",1)
+cv2.imshow("original",img1)
+
+print(img1.shape)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+import cv2
+
+
+img1 = cv2.imread("H:\data science\Image Processing\download.jpg",1)
+cv2.imshow("original",img1)
+
+print(img1.shape)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
